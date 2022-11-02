5d1bf76bf28431761a9dc546f55f761c1e154679
Joao Barbosa <joao.barbosa@glazedsolutions.com>
2022-11-02 12:08:04 +0000

    Avoid genreString being recreated everytime screen re-renders

--- src/components/MovieCell.tsx
+++ src/components/MovieCell.tsx
@@ -1,38 +1,10 @@
 import React from "react";
 import { View, Image, Text, StyleSheet } from 'react-native'
 import { Movie } from "../model/Movie";
-import { GenreId } from "../model/GenreId";
+import { genreString } from "../model/Movie";
 
 const MovieCell = (movie: Movie) => {
-
-    function genreName(genreId: GenreId) {
-        switch (genreId) {
-            case GenreId.action: return 'Action'                
-            case GenreId.animation: return 'Animation'
-            case GenreId.comedy: return 'Comedy'
-            case GenreId.crime: return 'Crime'
-            case GenreId.documentary: return 'Documentary'
-            case GenreId.drama: return 'Drama'
-            case GenreId.family: return 'Family'
-            case GenreId.fantasy: return 'Fantasy'
-            case GenreId.history: return 'History'
-            case GenreId.horror: return 'Horror'
-            case GenreId.music: return 'Music'
-            case GenreId.mystery: return 'Mystery'
-            case GenreId.romance: return 'Romance'
-            case GenreId.scienceFiction: return 'Science Fiction'
-            case GenreId.tvMovie: return 'TV Movie'
-            case GenreId.thriller: return 'Thriller'
-            case GenreId.war: return 'War'
-            case GenreId.western: return 'Western'
-            default:
-                return ''
-        }
-    }
     
-    
-    const genreString = movie.genre_ids ? movie.genre_ids.map(id => genreName(id)).join(", ") : ''
-
     return (
         <View style={styles.container}>
 
@@ -42,7 +14,7 @@ const MovieCell = (movie: Movie) => {
             />
             <View style={styles.textBox}>
                 <Text style={styles.title}>{movie.title}</Text>
-                <Text style={styles.description}> {movie.release_date} | {genreString}</Text>
+                <Text style={styles.description}> {movie.release_date} | {genreString(movie)}</Text>
             </View>
         </View>
     )
--- src/model/Movie.tsx
+++ src/model/Movie.tsx
@@ -8,4 +8,34 @@ export type Movie = {
     vote_average: number
     release_date?: string
     genre_ids?: [GenreId]
+
+}
+
+export function genreString(movie: Movie) {
+    return movie.genre_ids ? movie.genre_ids.map(id => genreName(id)).join(", ") : ''
+}
+
+function genreName(genreId: GenreId) {
+    switch (genreId) {
+        case GenreId.action: return 'Action'                
+        case GenreId.animation: return 'Animation'
+        case GenreId.comedy: return 'Comedy'
+        case GenreId.crime: return 'Crime'
+        case GenreId.documentary: return 'Documentary'
+        case GenreId.drama: return 'Drama'
+        case GenreId.family: return 'Family'
+        case GenreId.fantasy: return 'Fantasy'
+        case GenreId.history: return 'History'
+        case GenreId.horror: return 'Horror'
+        case GenreId.music: return 'Music'
+        case GenreId.mystery: return 'Mystery'
+        case GenreId.romance: return 'Romance'
+        case GenreId.scienceFiction: return 'Science Fiction'
+        case GenreId.tvMovie: return 'TV Movie'
+        case GenreId.thriller: return 'Thriller'
+        case GenreId.war: return 'War'
+        case GenreId.western: return 'Western'
+        default:
+            return ''
+    }
 }
